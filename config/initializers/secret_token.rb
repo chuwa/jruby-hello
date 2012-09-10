586039b86c02c0ca9cbaf39453e3aa2d4ea35e75
Samson Wu <samson959@gmail.com>
2012-09-10 15:10:43 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyHello::Application.config.secret_token = '469f8203e71297a98592461abdee1ec66e666b6f717d3abece7a7739205636f6282b387e22fa421d6055f578d6e5c6b3e078d452ac27f0a04e1d160b22abffd7'
